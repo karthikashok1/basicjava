59315e3ff23cb74cf1e0b8e409d4b3b2c97b9953
Karthik Ashok <pepsi,nike@gmail.com>
2017-08-08 18:18:52 -0700

    Homework

---
+++ homework/Homework4/NumbersHomework/src/Application.java
@@ -0,0 +1,12 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int x;
+			for(x=5; x<100; x=x+10) {
+				System.out.println(x);
+			}
+	}
+
+}
